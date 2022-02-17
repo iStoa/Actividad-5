8695c397d03ddba7f2ba6b97460855f2bc0b2ad9
iStoa <99360705+iStoa@users.noreply.github.com>
2022-02-16 20:33:57 -0600

    Add files via upload

---
+++ Act5.java
@@ -0,0 +1,21 @@
+public class Act5 {
+    public static void main(String[] args) {
+        double X = 15;
+        double Y = 26;
+        double suma, resta, multi, div, modulo;
+
+
+        suma = X+Y;
+        resta = X-Y;
+        multi = X*Y;
+        div = X / Y;
+        modulo = X % Y;
+
+
+        System.out.println("La suma de " +X+ " y "+Y+" es:"+suma);
+        System.out.println("La resta de " +X+ " y "+Y+" es: "+resta);
+        System.out.println("La multiplicacion de " +X+ " y " +Y+" es: "+multi);
+        System.out.println("La division de "+Y+" y "+X+" es: "+div);
+        System.out.println("El modulo de "+Y+" y "+X+" es: "+modulo);
+    }
+}
